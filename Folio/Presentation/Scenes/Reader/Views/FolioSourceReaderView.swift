23458cfb7cad67a36bd500c6ce12cfed4f13ce49
namnn <namnn@nustechnology.com>
2026-09-16 10:35:22 +0700

    fix: content card sizes to content instead of stretching to screen bottom

--- Folio/Presentation/Scenes/Reader/Views/FolioSourceReaderView.swift
+++ Folio/Presentation/Scenes/Reader/Views/FolioSourceReaderView.swift
@@ -197,16 +197,8 @@ struct FolioSourceReaderView: View {
     private var scrollableContentCard: some View {
         ScrollView {
             contentCard
+                .fixedSize(horizontal: false, vertical: true)
         }
-        .background(Color.folioSurfaceStrong)
-        .clipShape(UnevenRoundedRectangle(
-            topLeadingRadius: 16,
-            bottomLeadingRadius: 0,
-            bottomTrailingRadius: 0,
-            topTrailingRadius: 16
-        ))
-        .padding(.horizontal, 18)
-        .ignoresSafeArea(edges: .bottom)
     }
 
     private var contentCard: some View {
@@ -244,6 +236,9 @@ struct FolioSourceReaderView: View {
         .padding(.horizontal, 16)
         .padding(.top, 16)
         .padding(.bottom, 32)
+        .background(Color.folioSurfaceStrong)
+        .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+        .padding(.horizontal, 18)
     }
     
     // MARK: - Sheets
